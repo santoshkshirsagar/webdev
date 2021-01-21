46b2a546a16521e576885fc567d56f2347049a55
Santosh <santosh.kshirsagar@aptech.ac.in>
2021-01-21 21:49:43 +0530

    #18 - Jquery Basics, selector, hide/show, val, event

--- index.php
+++ index.php
@@ -3,9 +3,12 @@ session_start();
 $title="Home Page";
 include('header.php');
 ?>
+<style>
+.blueColor{color:blue;font-size:40px;}
+</style>
 <main>
         <div id="title" >
-            <h1 style="display:block;" id="textTitle" class="text-center py-4">Resume Maker<br/></h1>
+            <h1 style="display:block;" id="textTitle" class="text-center py-4 ">Resume Maker<br/></h1>
         <!--     <button onclick="changeColor()">Change Color</button> -->
         </div>
         <div class="featured">
@@ -15,7 +18,15 @@ include('header.php');
                         <img id="img" class="w-75" src="images/dp.png">
                     </div>
                     <div class="col-8">
-                        <h2>Welcome to Web Dev Course</h2>
+                        <h2  id="welcome">Welcome to Web Dev Course</h2>
+                        <div id="divTest" class="classheading" style="width:300px;height:200px;background-color:green;">original text</div>
+                        <button id="btntest">test</button>
+                        <button id="addt">add</button>
+                        <button id="removet">remove</button>
+                        
+                        <button id="csstest">css</button>
+                        <br/>
+                        <input id="inputText" type="text" class="form-control">
                         <p>HTML, PHP, Javascript etc</p>
                     </div>
                     
@@ -39,6 +50,34 @@ include('header.php');
             count=count+1;
         }
     </script>
+
+    <script>
+        //document.getElementById('welcome').innerHTML="cafasf";
+        window.onload=function(){
+            $("#btntest").click(function(){
+                $("#divTest").toggleClass('blueColor');
+                $("#divTest").width('600px');
+                $("#divTest").height('100px');
+                //$("#inputText").val("changed value");
+            })
+            $("#addt").click(function(){
+                $("#divTest").addClass('blueColor');
+            })
+            $("#removet").click(function(){
+                $("#divTest").removeClass('blueColor');
+            })
+
+            
+            $("#csstest").click(function(){
+                $("#divTest").css('font-size','100px');
+            })
+            $('#inputText').click(function(){
+                $(this).val('asfasdfasdf');
+            })
+
+        }
+    </script>
+
 <?php
 include('footer.php');
 ?>
